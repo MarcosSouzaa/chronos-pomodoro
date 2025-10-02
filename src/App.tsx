64d9ff11313a89622e69a212a905f40975467a60
Marcos Souza <marcossouza.git@gmail.com>
2025-10-02 20:30:04 -0300

    configuração da página com containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import { Heading } from './components/Heading';
-
 import './style/theme.css';
 import './style/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-        accusantium, laboriosam esse quo eveniet quasi praesentium ipsum sunt
-        minus possimus sit totam tempora dicta exercitationem blanditiis
-        adipisci omnis mollitia. Consectetur.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORMULÁRIO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
