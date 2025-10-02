380b815c1ff4355db5e68b093a135ce444a68d16
Marcos Souza <marcossouza.git@gmail.com>
2025-10-02 18:58:20 -0300

    estilizando o botão e o heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,17 @@ import { Heading } from './components/Heading';
 
 import './style/theme.css';
 import './style/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
-    //aqui, estou paasando props para o componente Heading
-    // o props é um objeto que guarda todas as propriedades que eu passo
-    //para o componente Heading
-    //o props.children guarda tudo que eu passo entre as tags do componente
-    //nesse caso, o texto "Olá Mundo!"
-    // posso também passar outras propriedades, como attr e attr2
-    //essas propriedades podem ser de qualquer tipo: número, string, boolean, objeto, array, função...
-    //dentro do componente Heading, eu posso acessar essas propriedades através do objeto props
-    //por exemplo, props.attr vai me dar o valor 123 e props.attr2 vai me dar o valor 'StringQualquer'
     <>
-      <Heading>Olá Mundo!</Heading>
+      <Heading>
+        Olá Mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
         accusantium, laboriosam esse quo eveniet quasi praesentium ipsum sunt
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,23 +1,10 @@
-/*Importando o css module. Esse .module faz com que o css tenha um escopo
-permite também eu usar uma variável pra pegar os valores das classes que
-estão nesse arquivo Heading.module.css. Vou criar um objeto Style para usar
-como uma variável*/
 import objStyles from './Heading.module.css';
 
-//Vou criar uma function e retornar um heading
-//Vou usar a variável objStyles para pegar a classe que eu quero usar
-//vou instalar uma extensão chamada "CSS Modules" para o VSCode
-//Essa extensão vai me ajudar a ver as classes que eu tenho disponíveis
-//no arquivo Heading.module.css
-//Vou usar a classe heading que eu criei no arquivo Heading.module.css
 type HeadingProps = {
-  children: string;
+  children: React.ReactNode; // ReactNode pode ser qualquer coisa que o React renderiza (string, number, elemento, array de elementos, fragmentos, etc)
 };
 
-export function Heading(props: HeadingProps) {
-  return (
-    //Posso usar template string para usar mais de uma classe
-    // <h1 className={`${objStyles.heading} ${objStyles.cyan}`}>Olá Mundo!</h1>
-    <h1 className={objStyles.heading}>{props.children}</h1>
-  );
+export function Heading({ children }: HeadingProps) {
+  //const {children} = props; estou desestruturando o props dentro do parametro da função
+  return <h1 className={objStyles.heading}>{children}</h1>;
 }
