b9cf8cf93be0de1ff727e956477d68772a788928
Marcos Souza <marcossouza.git@gmail.com>
2025-10-14 19:35:44 -0300

    Usando o useEffect para renderizar o efeito colateral

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react'; //usando o Hoock dos Efeitos Colaterais
 
 /*Usando Estado e criando um tipo diferente de String, dark ou light */
 /*Criei no useState e removi pra cá criando um tipo AvailableTheme */
@@ -14,17 +14,43 @@ export function Menu() {
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) {
     event.preventDefault(); //Não siga o link mudando de página
-    console.log('Clicado', Date.now());
+
     /*Vou pegar da memória o tema interno que está nela. prevTheme é o tema anterior
 vou criar a função nextTheme (qual é o próximo tema, que é o contrário do atual
 então, se prevTheme for igual a 'dark', ele será(?), senão (:), pode voltar para 
 'dark' e returno o nextTheme que será configurado lá encima */
     setTheme(prevTheme => {
       const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
-      document.documentElement.setAttribute('data-theme', theme);
       return nextTheme;
     });
   }
+
+  /* Três formas de usar EFEITOS COLATERAIS
+   ----- Passando uma função
+   Executa todas as vezes que o componente renderiza na tela
+   useEffect(() => {
+     console.log('useEffect sem dependências', Date.now());
+   });*/
+
+  /*----- Passando um array vazio (array de dependência), como segundo parâmetro
+  Executa apenas quando o react monta o componente na tela pela primeira vez
+  Ideal para ecommerce com API externa
+  useEffect(() => {
+  console.log('useEfects com array de dependências vazio', Date.now());
+  }, []);*/
+
+  /*----- Passando um array com parâmetro, ou seja, uma dependência como segundo parâmetro
+  Quando eu der o setTheme vou executar essa função e renderizar o Tema */
+  useEffect(() => {
+    console.log('Theme mudou', theme, Date.now());
+    document.documentElement.setAttribute('data-theme', theme);
+
+    /* Função que limpa restos deixados e atualiza o componente
+    antes de mudar o tema */
+    return () => {
+      console.log('Olha, este componente srá atualizado!');
+    };
+  }, [theme]);
 
   return (
     <nav className={styles.menu}>
