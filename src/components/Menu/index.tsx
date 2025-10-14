2fd804656e559c1ef27f0f48b4c05a8da32fc28e
Marcos Souza <marcossouza.git@gmail.com>
2025-10-14 20:43:14 -0300

    Salvando o tema no local Storage e trocando o ícone claro e escuro

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,10 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 import styles from './styles.module.css';
 import { useState, useEffect } from 'react'; //usando o Hoock dos Efeitos Colaterais
 
@@ -7,46 +13,36 @@ import { useState, useEffect } from 'react'; //usando o Hoock dos Efeitos Colate
 type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
-  /*Usando Estado e criando um tipo diferente de String, dark ou light */
-  const [theme, setTheme] = useState<AvailableThemes>('dark');
+  /* Buscando a chave do tema que salvei no local storage */
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+    return storageTheme;
+    /* ou 
+    return storageTheme === 'light' ? 'light : 'dark';  */
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) {
-    event.preventDefault(); //Não siga o link mudando de página
+    event.preventDefault();
 
-    /*Vou pegar da memória o tema interno que está nela. prevTheme é o tema anterior
-vou criar a função nextTheme (qual é o próximo tema, que é o contrário do atual
-então, se prevTheme for igual a 'dark', ele será(?), senão (:), pode voltar para 
-'dark' e returno o nextTheme que será configurado lá encima */
     setTheme(prevTheme => {
       const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
       return nextTheme;
     });
   }
 
-  /* Três formas de usar EFEITOS COLATERAIS
-   ----- Passando uma função
-   Executa todas as vezes que o componente renderiza na tela
-   useEffect(() => {
-     console.log('useEffect sem dependências', Date.now());
-   });*/
-
-  /*----- Passando um array vazio (array de dependência), como segundo parâmetro
-  Executa apenas quando o react monta o componente na tela pela primeira vez
-  Ideal para ecommerce com API externa
   useEffect(() => {
-  console.log('useEfects com array de dependências vazio', Date.now());
-  }, []);*/
-
-  /*----- Passando um array com parâmetro, ou seja, uma dependência como segundo parâmetro
-  Quando eu der o setTheme vou executar essa função e renderizar o Tema */
-  useEffect(() => {
-    console.log('Theme mudou', theme, Date.now());
     document.documentElement.setAttribute('data-theme', theme);
+    //Salvando chave 'theme' e valor theme no local Storage do navegador
+    localStorage.setItem('theme', theme);
 
-    /* Função que limpa restos deixados e atualiza o componente
-    antes de mudar o tema */
     return () => {
       console.log('Olha, este componente srá atualizado!');
     };
@@ -54,7 +50,6 @@ então, se prevTheme for igual a 'dark', ele será(?), senão (:), pode voltar p
 
   return (
     <nav className={styles.menu}>
-      <h1>{theme}</h1>
       <a
         className={styles.menuLink}
         href='#'
@@ -86,7 +81,11 @@ então, se prevTheme for igual a 'dark', ele será(?), senão (:), pode voltar p
         title='Mudar Tema'
         onClick={handleThemeChange}
       >
-        <SunIcon />
+        {nextThemeIcon[theme]}
+
+        {/*Aqui eu poderia fazer if/else, mas preferi fazer um objeto com
+        chave e valor lá encima e usar aqui passando o theme:
+        const nextThemeIcon = {dark: <SunIcon />,light: <MoonIcon />,};*/}
       </a>
     </nav>
   );
