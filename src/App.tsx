edf9fa7de5ed0cd4f8d9f2b936ba0dd411b9c000
Marcos Souza <marcossouza.git@gmail.com>
2025-10-16 20:43:03 -0300

    Criação de Componentes GenericHtml para páginas que renderizam HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,8 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDow } from './components/CountDown';
-import { DefaltInput } from './components/DefaltInput';
-import { Cycles } from './components/Cycles';
-import { DefaltButton } from './components/DefaltButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
 
 import './style/theme.css';
 import './style/global.css';
 
 export function App() {
-  return (
-    <>
-      S
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDow />
-      </Container>
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            {/*No input geralmente vamos configurar coisas como type etc. Vou fazer isso no Componente
-  DefaltInput/index.tsx*/}
-            <DefaltInput
-              labelText='Task'
-              id='meuInput'
-              type='text'
-              placeholder='Digite algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Nesse ciclo descanse por 5 min.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaltButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,33 @@
+import { PlayCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaltButton } from '../DefaltButton';
+import { DefaltInput } from '../DefaltInput';
+
+export function MainForm() {
+  return (
+    <form className='form' action=''>
+      <div className='formRow'>
+        {/*No input geralmente vamos configurar coisas como type etc.
+             Vou fazer isso no Componente DefaltInput/index.tsx*/}
+        <DefaltInput
+          labelText='Task'
+          id='meuInput'
+          type='text'
+          placeholder='Digite algo'
+        />
+      </div>
+
+      <div className='formRow'>
+        <p>Nesse ciclo descanse por 5 min.</p>
+      </div>
+
+      <div className='formRow'>
+        <Cycles />
+      </div>
+
+      <div className='formRow'>
+        <DefaltButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+}
